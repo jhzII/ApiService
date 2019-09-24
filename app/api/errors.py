e424dc68753fdb529fe53a594e3fa5946fbc2463
Boris Gomziakov <jhzll@yandex.ru>
2019-09-24 19:54:51 +0300

    Добавлен метод составления словаря со всевозможными кодами ошибок.

--- app/api/errors.py
+++ app/api/errors.py
@@ -4,6 +4,12 @@ from flask import make_response, jsonify
 class ApiError(Exception):
     """ Общее api исключение. """
 
+    http_code = None
+    api_code = None
+    message = None
+
+    errors = []
+
     def __init__(self, message, http_code, api_code):
         self.http_code = http_code
         self.api_code = api_code
@@ -11,77 +17,127 @@ class ApiError(Exception):
 
     def make_response(self):
         return make_response(jsonify({
-            # 'status': self.api_code,
             'message': self.message,
             'code': self.api_code
         }), self.http_code)
+
+    def __init_subclass__(cls, **kwargs):
+        """ Переписывает всевозможные коды ошибок в словарь при каждом новом наследовании. """
+
+        cls.errors.append({
+            'http_code': cls.http_code,
+            'api_code': cls.api_code,
+            'message': cls.message
+        })
+
+        super().__init_subclass__(**kwargs)
 
 
 class AlreadyConfirmedError(ApiError):
     """ Исключение. Повторное подтверждение. """
 
-    def __init__(self, message='Email has already been confirmed.'):
-        super().__init__(message, http_code=400, api_code=1000)
+    http_code = 400
+    api_code = 1000
+    message = 'Email has already been confirmed.'
+
+    def __init__(self, message=message):
+        super().__init__(message, http_code=self.http_code, api_code=self.api_code)
 
 
 class InvalidLincError(ApiError):
     """ Исключение. Недействительная ссылка. """
 
-    def __init__(self, message='Invalid link.'):
-        super().__init__(message, http_code=400, api_code=1001)
+    http_code = 400
+    api_code = 1001
+    message = 'Invalid link.'
+
+    def __init__(self, message=message):
+        super().__init__(message, http_code=self.http_code, api_code=self.api_code)
 
 
 class InvalidTokenError(ApiError):
     """ Исключение. Недействительный токен. """
 
-    def __init__(self, message='Invalid token.'):
-        super().__init__(message, http_code=400, api_code=1002)
+    http_code = 400
+    api_code = 1002
+    message = 'Invalid token.'
+
+    def __init__(self, message=message):
+        super().__init__(message, http_code=self.http_code, api_code=self.api_code)
 
 
 class NotConfirmedError(ApiError):
     """ Исключение. Отсутствует подтверждение. """
 
-    def __init__(self, message='Email not confirmed.'):
-        super().__init__(message, http_code=400, api_code=1003)
+    http_code = 400
+    api_code = 1003
+    message = 'Email not confirmed.'
+
+    def __init__(self, message=message):
+        super().__init__(message, http_code=self.http_code, api_code=self.api_code)
 
 
 class WrongDataError(ApiError):
     """ Исключение. Неверные данные. """
 
-    def __init__(self, message='Wrong username or password.'):
-        super().__init__(message, http_code=400, api_code=1004)
+    http_code = 400
+    api_code = 1004
+    message = 'Wrong username or password.'
+
+    def __init__(self, message=message):
+        super().__init__(message, http_code=self.http_code, api_code=self.api_code)
 
 
 class RightsError(ApiError):
     """ Исключение. Недостаточно прав. """
 
-    def __init__(self, message='Insufficient rights.'):
-        super().__init__(message, http_code=400, api_code=1005)
+    http_code = 400
+    api_code = 1005
+    message = 'Insufficient rights.'
+
+    def __init__(self, message=message):
+        super().__init__(message, http_code=self.http_code, api_code=self.api_code)
 
 
 class NotFoundError(ApiError):
     """ Исключение. Не найдено. """
 
-    def __init__(self, message='Not found.'):
-        super().__init__(message, http_code=404, api_code=1006)
+    http_code = 404
+    api_code = 1006
+    message = 'Not found.'
+
+    def __init__(self, message=message):
+        super().__init__(message, http_code=self.http_code, api_code=self.api_code)
 
 
 class InsufficientDataError(ApiError):
     """ Исключение. Не все данные переданы. """
 
-    def __init__(self, message='Insufficient data.'):
-        super().__init__(message, http_code=400, api_code=1007)
+    http_code = 400
+    api_code = 1007
+    message = 'Insufficient data.'
+
+    def __init__(self, message=message):
+        super().__init__(message, http_code=self.http_code, api_code=self.api_code)
 
 
 class NameUsedError(ApiError):
     """ Исключение. Переданные данные уже были испольщованы. """
 
-    def __init__(self, message='Please use a different username.'):
-        super().__init__(message, http_code=400, api_code=1008)
+    http_code = 400
+    api_code = 1008
+    message = 'Please use a different username.'
+
+    def __init__(self, message=message):
+        super().__init__(message, http_code=self.http_code, api_code=self.api_code)
 
 
 class EmailUsedError(ApiError):
     """ Исключение. Переданные данные уже были испольщованы. """
 
-    def __init__(self, message='Please use a different email.'):
-        super().__init__(message, http_code=400, api_code=1009)
+    http_code = 400
+    api_code = 1009
+    message = 'Please use a different email.'
+
+    def __init__(self, message=message):
+        super().__init__(message, http_code=self.http_code, api_code=self.api_code)
